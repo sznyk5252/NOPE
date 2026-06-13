2cda8fe5c150c787e462575026a6cd4496629381
skrezolek <szymek.krezolek@gmail.com>
2026-06-13 10:51:08 +0200

    upd

--- src/nope/__init__.py
+++ src/nope/__init__.py
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
 from .api import compile_from_string
 
 __all__ = ["compile_from_string"]
+=======
+from .api import compile_from_string, compile_from_file
+__all__ = ["compile_from_string", "compile_from_file"]
+>>>>>>> Stashed changes
