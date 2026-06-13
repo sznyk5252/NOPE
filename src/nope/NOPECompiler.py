8c8bba49a96223d67278a279fe8a5d42848f7305
skrezolek <szymek.krezolek@gmail.com>
2026-06-14 00:00:00 +0200

    upd

--- src/nope/NOPECompiler.py
+++ src/nope/NOPECompiler.py
@@ -32,10 +32,10 @@ class NopeCompiler(NOPEVisitor):
         full_code.extend(self.global_scope)
         full_code.append("int main(){")
 
+        full_code.append("nope_expect_eof();")
         self.main_scope.append("nope_cleanup();")
         for line in self.main_scope:
             full_code.append(f"\t{line}")
-        full_code.append("\tnope_expect_eof();")
         full_code.append("\treturn 0;\n}")
 
         return "\n".join(full_code)
